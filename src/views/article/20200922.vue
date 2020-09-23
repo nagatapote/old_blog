3d5a9beebdb2a082a9880df04644db6bc23d5cea
nagatapote <hiroaki29p@icloud.com>
2020-09-23 19:14:27 +0900

    blog

--- src/views/article/20200922.vue
+++ src/views/article/20200922.vue
@@ -1,27 +1,27 @@
 <template>
     <div class="20200922" id="article1">
-        <h1>vue.jsとnetlifyでblog作成</h1>
-        <h2>■目的</h2>
+        <h2>vue.jsとnetlifyでblog作成</h2>
+        <h3>■目的</h3>
         <p>技術ブログ作成し、やったことを残しておくため</p>
-        <h2>■使ったもの</h2>
+        <h3>■使ったもの</h3>
         <p>PC：MacBookPro 13インチ メモリ8GB</p>
         <p>エディタ：Visual Studio Code(ターミナルも使えて便利)</p>
-        <h2>■やったこと</h2>
+        <h3>■やったこと</h3>
         <p>・vue.jsとgithubの連携</p>
         <p>・githubとnetlifyの連携</p>
-        <h2>■vue.jsとgithubの連携</h2>
-        <h3>▼githubの登録、リポジトリの作成</h3>
+        <h3>■vue.jsとgithubの連携</h3>
+        <h4>▼githubの登録、リポジトリの作成</h4>
         <p>githubとググって、登録</p>
         <img id="img" src="../../assets/20200922/NewRepositories.png">
         <p>ログイン後、左上の「New」をクリック</p>
         <img id="img" src="../../assets/20200922/CreateRepositories.png">
         <p>「Repository name」に好きな名前を入れて、「Create repository」をクリックすれば完了です。</p>
-        <h3>▼公開鍵・秘密鍵の作成</h3>
+        <h4>▼公開鍵・秘密鍵の作成</h4>
         <b>鍵を入れるフォルダへ移動</b>
         <p>$ cd ~/.ssh</p>
         <b>鍵を生成</b>
         <p>$ ssh-keygen -t rsa (質問されるが、全てEnter)</p>
-        <h3>▼公開鍵をGitHubにアップ</h3>
+        <h4>▼公開鍵をGitHubにアップ</h4>
         <a href="https://github.com/settings/ssh">https://github.com/settings/ssh</a><br>
         <p>上記ページで公開鍵の設定が可能。（GitHubに登録してある必要があります。）</p>
         <img id="img" src="../../assets/20200922/SSHkey.png">
@@ -35,7 +35,7 @@
         <p>以下のように表示されたら接続完了</p>
         <p>You've successfully authenticated, but GitHub does not provide shell access.</p>
 
-        <h3>▼githubへpushする方法</h3>
+        <h4>▼githubへpushする方法</h4>
         <b>作業ディレクトリをgithubで管理するためのコマンド</b>
         <p>$ git init</p>
         <b>作業ディレクトリ→ステージングエリア</b>
@@ -50,7 +50,7 @@
         <p>以下の「Code」で確認可能</p>
         <img id="img" src="../../assets/20200922/githubCode.png">
         <p>$ git push -u origin master</p>
-        <h2>■githubとnetlifyの連携</h2>
+        <h3>■githubとnetlifyの連携</h3>
         <p>netlifyとググって、githubでログイン</p>
         <img id="img" src="../../assets/20200922/NewSiteFromGit.png">
         <p>右上の「New site from Git」をクリック</p>
