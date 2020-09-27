e7f26c0235ec7b7337fe6258bb65b0fa56a6da81
nagatapote <hiroaki29p@icloud.com>
2020-09-27 19:02:04 +0900

    article4

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,6 +9,35 @@
     <v-img
       class="white--text align-end"
       height="200px"
+      src="https://cdn.pixabay.com/photo/2017/04/09/09/56/avenue-2215317_960_720.jpg"
+    >
+    <v-card-title></v-card-title>
+    </v-img>
+
+    <v-card-subtitle class="pb-0">2020/09/27</v-card-subtitle>
+
+    <v-card-text class="text--primary">
+      <div>vue.jsとfirebaseで認証機能実装</div>
+    </v-card-text>
+    <v-card-actions>
+      <v-btn
+        color="orange"
+        text
+        to="/article4"
+      >
+        ReadMore
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+  <br><br>
+
+<v-card
+    class="mx-auto"
+    max-width="400"
+  >
+    <v-img
+      class="white--text align-end"
+      height="200px"
       src="https://cdn.pixabay.com/photo/2016/08/11/23/48/italy-1587287_960_720.jpg"
     >
     <v-card-title></v-card-title>
