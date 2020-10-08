0aa7468601887fa6a7d9a718548aa03feec25774
nagatapote <hiroaki29p@icloud.com>
2020-10-08 16:51:21 +0900

    blog

--- src/main.js
+++ src/main.js
@@ -3,13 +3,6 @@ import App from './App.vue'
 import router from './router'
 import Ads from 'vue-google-adsense'
 import vuetify from './plugins/vuetify'
-import axios from 'axios'
-import 'highlight.js/styles/default.css'
-import Highlight from 'vue-markdown-highlight'
-
-Vue.use(Highlight)
-Vue.config.productionTip = false
-Vue.prototype.$axios = axios
 
 /* アドセンスの使用宣言 */
 Vue.use(require('vue-script2'))
