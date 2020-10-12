86d63aa59b2ebe6c1f90e6436655303be6e725c7
nagatapote <hiroaki29p@icloud.com>
2020-10-13 08:31:34 +0900

    form

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,38 +1,13 @@
 <template>
-  <form
-    name="ask-question"
-    method="post"
-    data-netlify="true"
-    data-netlify-honeypot="bot-field"
-    >
-    <input type="hidden" name="form-name" value="ask-question" />
-    <label v-for="(panelist, index) in panelists" :key="index">
-      <input
-        type="radio"
-        name="panelist"
-        :value="panelist"
-        @input="ev => updatePanelist"
-        :checked="panelist === currentPanelist"
-      />
-      <span>{{ panelist }}</span>
-    </label>
-    ...
-    <button>Submit</button>
+  <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
+    <input type="hidden" name="form-name" value="contact" />
+    <label>Your Name: <input type="text" name="name" /></label>
+    <label>Your Email: <input type="email" name="email" /></label>
+    <label>Your Role: <select name="role[]" multiple>
+      <option value="leader">Leader</option>
+      <option value="follower">Follower</option>
+    </select></label>
+    <label>Message: <textarea name="message"></textarea></label>
+    <button type="submit">Send</button>
   </form>
 </template>
-<script>
-export default {
-  name: 'QAForm',
-  methods: {
-    updatePanelist (ev) {
-      this.currentPanelist = ev.target.value
-    }
-  },
-  data () {
-    return {
-      panelists: ['Evan You', 'Chris Fritz'],
-      currentPanelist: 'Evan You'
-    }
-  }
-}
-</script>
