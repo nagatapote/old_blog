e52c29270fe8a2996a025798129aa6dfdacde13a
nagatapote <hiroaki29p@icloud.com>
2020-10-13 08:20:50 +0900

    form

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,55 +1,37 @@
 <template>
-<div id='form'>
-<form name="contact" method="POST" netlify>
-  <p>
-    <label>Your Name: <input type="text" name="name" /></label>
-  </p>
-  <p>
-    <label>Your Email: <input type="email" name="email" /></label>
-  </p>
-  <p>
-    <label>Your Role: <select name="role[]" multiple>
-      <option value="leader">Leader</option>
-      <option value="follower">Follower</option>
-    </select></label>
-  </p>
-  <p>
-    <label>Message: <textarea name="message"></textarea></label>
-  </p>
-  <p>
-    <button type="submit">Send</button>
-  </p>
-</form>
-</div>
+  <form
+    name="ask-question"
+    method="post"
+    data-netlify="true"
+    data-netlify-honeypot="bot-field"
+    >
+    <input type="hidden" name="form-name" value="ask-question" />
+    <label v-for="(panelist, index) in panelists" :key="index">
+      <input
+        type="radio"
+        name="panelist"
+        :value="panelist"
+        @input="ev => updatePanelist"
+        :checked="panelist === currentPanelist"
+      />
+      <span>{{ panelist }}</span>
+    </label>
+    ...
+    <button>Submit</button>
+  </form>
 </template>
-
 <script>
-import axios from 'axios'
-
 export default {
-  name: '#form',
-  data () {
-    return {
-      form: {
-        askPerson: ''
-      }
+  name: 'QAForm',
+  methods: {
+    updatePanelist (ev) {
+      this.currentPanelist = ev.target.value
     }
   },
-  methods: {
-    Submit () {
-      axios.post('/',
-        this.encode({
-          "form-name": "contact",
-          ...this.form
-        }),
-        axiosConfig
-        )
-        .then(() => {
-          this.$router.push('thanks')
-        })
-        .catch(() => {
-          this.$router.push('404')
-        })
+  data () {
+    return {
+      panelists: ['Evan You', 'Chris Fritz'],
+      currentPanelist: 'Evan You'
     }
   }
 }
