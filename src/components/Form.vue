694d2f1e5f001a2b56bde4adbc0aba0ac7f70c35
nagatapote <hiroaki29p@icloud.com>
2020-10-13 12:53:55 +0900

    form_test

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -4,15 +4,15 @@
   <input type="hidden" name="form-name" value="contact" />
   <div class="Form-Item">
     <p class="Form-Item-Label"><span class="Form-Item-Label-Required">必須</span>氏名</p>
-    <input type="text" class="Form-Item-Input" placeholder="例）山田太郎">
+    <input type="text" name="name" class="Form-Item-Input" placeholder="例）山田太郎">
   </div>
   <div class="Form-Item">
     <p class="Form-Item-Label"><span class="Form-Item-Label-Required">必須</span>メールアドレス</p>
-    <input type="email" class="Form-Item-Input" placeholder="例）example@gmail.com">
+    <input type="email" name="email" class="Form-Item-Input" placeholder="例）example@gmail.com">
   </div>
   <div class="Form-Item">
     <p class="Form-Item-Label isMsg"><span class="Form-Item-Label-Required">必須</span>お問い合わせ内容</p>
-    <textarea class="Form-Item-Textarea"></textarea>
+    <textarea name="message" class="Form-Item-Textarea"></textarea>
   </div>
   <input type="submit" class="Form-Btn" value="送信する">
 </form>
