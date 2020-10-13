705617dd847ead7e380123f717d39a3e4b0216f0
nagatapote <hiroaki29p@icloud.com>
2020-10-13 09:11:22 +0900

    form_test

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,5 +1,6 @@
 <template>
   <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
+    <input type="hidden" name="form-name" value="contact" />
     <label>Your Name: <input type="text" name="name" /></label>
     <label>Your Email: <input type="email" name="email" /></label>
     <label>Message: <textarea name="message"></textarea></label>
