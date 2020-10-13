65ebff31d498a0fa9aab262a7a25a861994c7660
nagatapote <hiroaki29p@icloud.com>
2020-10-13 12:06:56 +0900

    form_test

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,11 +1,84 @@
 <template>
 <div>
-  <form name="contact" method="POST" data-netlify="true">
-  <input type="hidden" name="form-name" value="contact" />
-  <label>Your Name: <input type="text" name="name" /></label>
-  <label>Your Email: <input type="email" name="email" /></label>
-  <label>Message: <textarea name="message"></textarea></label>
-  <button type="submit">Send</button>
-</form>
-</div>
+  <v-container tag="contact">
+    <v-row>
+      <v-col
+        v-text="title.toUpperCase()"
+        cols="12"
+        tag="h1"
+      />
+    </v-row>
+    <v-form name="contact" method="post" netlify data-netlify="true">
+      <v-text-field
+        v-show="false"
+        v-model="title"
+        name="form-name"
+        value="contact"
+      />
+      <v-row>
+        <v-col cols="12">
+          <v-text-field
+            v-model="name"
+            label="name"
+            name="name"
+            autofocus
+          />
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12">
+          <v-text-field
+            v-model="email"
+            label="E-mail"
+            name="email"
+          />
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12">
+          <v-textarea
+            v-model="message"
+            label="message"
+            name="message"
+          />
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <v-btn
+            :disabled="isEmpty"
+            type="submit"
+          >
+            submit
+          </v-btn>
+        </v-col>
+      </v-row>
+    </v-form>
+  </v-container>
+  </div>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      title: 'contact',
+      name: '',
+      email: '',
+      message: '' 
+    }
+  },
+  computed: {
+    isEmpty () {
+      if (
+        this.name !== '' &&
+        this.email !== '' &&
+        this.message !== '' 
+      ) {
+        return false
+      }
+      return true
+    }
+  }
+}
+</script>
