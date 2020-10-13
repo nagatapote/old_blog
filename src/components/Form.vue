09aea88ac709453e75a81a3d84df78306eaa26c9
nagatapote <hiroaki29p@icloud.com>
2020-10-13 14:13:47 +0900

    form_test

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,85 +1,85 @@
 <template>
-  <v-container tag="section">
-    <v-row>
-      <v-col
-        v-text="title.toUpperCase()"
-        cols="12"
-        tag="h1"
-      />
-    </v-row>
-    <v-form method="post" netlify>
-      <v-text-field
-        v-show="false"
-        v-model="title"
-        name="form-name"
-      />
-      <v-row>
-        <v-col cols="12">
-          <v-text-field
-            v-model="name"
-            label="name"
-            name="name"
-            autofocus
-          />
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col cols="12">
-          <v-text-field
-            v-model="email"
-            :rules="emailRules"
-            label="E-mail"
-            name="email"
-          />
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col cols="12">
-          <v-textarea
-            v-model="message"
-            label="message"
-            name="message"
-          />
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col>
-          <v-btn
-            :disabled="isEmpty"
-            type="submit"
-          >
-            submit
-          </v-btn>
-        </v-col>
-      </v-row>
-    </v-form>
-  </v-container>
+<div>
+  <v-col
+    v-text="title.toUpperCase()"
+    cols="12"
+    tag="h1"
+  />
+  <v-form
+    ref="form"
+    name="contact"
+    method="post"
+    netlify
+    data-netlify="true"
+    v-model="valid"
+    lazy-validation
+  >
+    <v-text-field
+      v-model="name"
+      :counter="10"
+      :rules="nameRules"
+      label="Name"
+      name="name"
+      required
+    ></v-text-field>
+
+    <v-text-field
+      v-model="email"
+      :rules="emailRules"
+      label="E-mail"
+      name="email"
+      required
+    ></v-text-field>
+
+    <v-textarea
+      v-model="message"
+      label="message"
+      name="message"
+    />
+
+    <v-btn
+      type="submit"
+      :disabled="!valid"
+      color="success"
+      class="mr-4"
+      @click="validate"
+    >
+      Submit
+    </v-btn>
+
+    <v-btn
+      color="error"
+      class="mr-4"
+      @click="reset"
+    >
+      Reset Form
+    </v-btn>
+  </v-form>
+  </div>
 </template>
 
 <script>
 export default {
-  data () {
-    return {
-      title: 'contact',
-      name: '',
-      email: '',
-      message: '',
-      emailRules: [
-        v => !!v || 'E-mail is required',
-        v => /.+@.+\..+/.test(v) || 'E-mail must be valid'
-      ]
-    }
-  },
-  computed: {
-    isEmpty () {
-      if (
-        this.name !== '' &&
-        this.email !== '' &&
-        this.message !== ''
-      ) {
-        return false
-      }
-      return true
+  data: () => ({
+    title: 'contact',
+    valid: true,
+    name: '',
+    nameRules: [
+      v => !!v || 'Name is required',
+      v => (v && v.length <= 10) || 'Name must be less than 10 characters'
+    ],
+    email: '',
+    emailRules: [
+      v => !!v || 'E-mail is required',
+      v => /.+@.+\..+/.test(v) || 'E-mail must be valid'
+    ]
+  }),
+  methods: {
+    validate () {
+      this.$refs.form.validate()
+    },
+    reset () {
+      this.$refs.form.reset()
     }
   }
 }
