f7a945eae3dac461142f8d59c6700d606df01ffa
nagatapote <hiroaki29p@icloud.com>
2020-10-13 11:59:25 +0900

    form_test

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,8 +1,8 @@
 <template>
 <div>
   <form name="contact" method="POST" data-netlify="true">
-  <input type="hidden" name="form-name" value="contact" /> 
-  <label>Your Name: <input type="text" name="name" /></label>   
+  <input type="hidden" name="form-name" value="contact" />
+  <label>Your Name: <input type="text" name="name" /></label>
   <label>Your Email: <input type="email" name="email" /></label>
   <label>Message: <textarea name="message"></textarea></label>
   <button type="submit">Send</button>
