8dfee23db9601b4840cdbf5e644bba4137857360
nagatapote <hiroaki29p@icloud.com>
2020-10-14 18:46:40 +0900

    blog

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,8 @@
 <template>
 <div>
 <header>
-  <h1>pote's blog</h1>
-<input type="checkbox" id="cp_navimenuid">
-<label class="menu" for="cp_navimenuid">
-<div class="menubar">
-Menu
-</div>
-
-<ul>
+<h1>pote's blog</h1>
+<ul class="topnav">
   <li><router-link to="/">Home</router-link></li>
   <li><router-link to="/About">Profile</router-link></li>
   <li><router-link to="/Article_1">Article</router-link></li>
@@ -16,7 +10,6 @@ Menu
   <li><a target="_blank" href="https://twitter.com/pote_nagata">Twitter</a></li>
   <li><a target="_blank" href="https://github.com/nagatapote">Github</a></li>
 </ul>
-</label>
 </header>
 <router-view />
 <footer>
@@ -27,15 +20,16 @@ Menu
 
 <style>
 body{
-  padding-top: 160px;
+  padding-top: 140px;
   padding-bottom: 75px
 }
 header{
-  padding: 10px;
+  padding: 5px;
   position: fixed;
   width: 100%;
   top: 0;
-  background-color: white
+  background-color: #1b2538;
+  color: white
 }
 footer{
   margin-top: 20px;
@@ -46,73 +40,28 @@ footer{
   width: 100%;
   bottom: 0
 }
-*, *:before, *:after {
-  -webkit-box-sizing: border-box;
-          box-sizing: border-box
-}
-.menu {
-  display: block;
+ul.topnav {
   overflow: hidden;
-  width: 150px;
-  height: 30px;
-  margin: 0px auto;
-  cursor: pointer;
-  -webkit-transition: all 0.5s ease-in-out;
-          transition: all 0.5s ease-in-out;
-  border: 3px solid transparent;
-  background-color: #1b2538;
-}
-.menubar{
-  color: white;
-  text-align: center
-}
-.menu ul {
-  display: none;
   margin: 0;
   padding: 0;
-  -webkit-transition: all 0.5s ease;
-          transition: all 0.5s ease;
-  text-align: center;
-  opacity: 0
+  list-style-type: none;
+  background-color: #1b2538;
 }
-.menu ul li {
-  display: inline-block
+ul.topnav li {
+  float: left
 }
-.menu ul li a {
-  display: inline-block;
-  padding: 0.7em 0.5em;
-  -webkit-transition: all 0.3s ease-in;
-          transition: all 0.3s ease-in;
-  text-decoration: none;
-  color: #1b2538;
-  border-bottom: 4px solid transparent;
-  height: 56px;
-  line-height: 2em
-}
-.menu ul li a:hover {
-  color: #ffffff;
-  background-color: #1b2538
-}
-.menu ul li a:target {
-  border-bottom-color: #1b2538
-}
-/* クリックした時の動き */
-#cp_navimenuid {
-  display: none
-}
-#cp_navimenuid:checked ~ .menu {
-  width: 100%;
-  height: 60px;
-  border: 3px solid #1b2538;
-  border-radius: 5px;
-  background-color: transparent
-}
-#cp_navimenuid:checked ~ .menu > ul {
+ul.topnav li a {
   display: block;
-  opacity: 1
+  padding: 10px 10px;
+  text-align: center;
+  text-decoration: none;
+  color: white
 }
-#cp_navimenuid:checked ~ .menu > .menubar {
-  display: none
+ul.topnav li a:hover:not(.active) {
+  background-color: #a9bce2
+}
+ul.topnav li a.active {
+  background-color: #da3c41
 }
 
 </style>
