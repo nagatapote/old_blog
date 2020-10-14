098a88cec821f3797d41b78f2276d0da376a8c28
nagatapote <hiroaki29p@icloud.com>
2020-10-14 14:55:10 +0900

    blog

--- src/views/Markdown.vue
+++ src/views/Markdown.vue
@@ -5,13 +5,7 @@
 </template>
 
 <script>
-import Vue from 'vue'
 import VueMarkdown from 'vue-markdown'
-import axios from 'axios'
-import Highlight from 'vue-markdown-highlight'
-
-Vue.prototype.$axios = axios
-Vue.use(Highlight)
 
 export default {
   el: '#md',
