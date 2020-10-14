76fbf0e26597850000ad07e13ed676153f533324
nagatapote <hiroaki29p@icloud.com>
2020-10-14 16:43:10 +0900

    blog

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import App from './App.vue'
-import Ads from 'vue-google-adsense'
 import router from './router'
 import axios from 'axios'
 import Highlight from 'vue-markdown-highlight'
@@ -9,12 +8,6 @@ Vue.prototype.$axios = axios
 Vue.use(Highlight)
 
 Vue.config.productionTip = false
-
-/* アドセンスの使用宣言 */
-Vue.use(require('vue-script2'))
-Vue.use(Ads.Adsense)
-Vue.use(Ads.InArticleAdsense)
-Vue.use(Ads.InFeedAdsense)
 
 new Vue({
   router,
