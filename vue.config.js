b53e6eb6e2a8ecb3ec3dae7ccba7ccbc0236344e
nagatapote <hiroaki29p@icloud.com>
2020-10-18 16:02:41 +0900

    pwa

--- vue.config.js
+++ vue.config.js
@@ -7,26 +7,24 @@ module.exports = {
       title: 'pote\'s blog',
     }
   },
-  pwa: {
-    name: 'pote\'s blog',
-    themeColor: '#5e21f3',
-    appleMobileWebAppCapable: 'yes',
-    appleMobileWebAppStatusBarStyle: 'black',
-
-    // configure the workbox plugin
-    workboxPluginMode: 'GenerateSW',
-    workboxOptions: {
-      // swSrc is required in InjectManifest mode.
-      // swSrc: 'dev/sw.js'
-      // ...other Workbox options...
-    }
-  },
   configureWebpack: config => {
     config.plugins.push(
       new GenerateSW({
         cacheId: 'calc-tax-insurance-v3',
-        skipWaiting: false,
-        clientsClaim: false
+        runtimeCaching: [{
+          urlPattern: /^https:\/\/nagatapote\.work/,
+          handler: 'staleWhileRevalidate',
+          options: {
+            cacheName: 'api',
+            expiration: {
+              maxEntries: 20,
+              maxAgeSeconds: 60 * 60 * 24 * 30
+            },
+            cacheableResponse: {
+              statuses: [0, 200]
+            }
+          }
+        }]
       })
     )
   }
