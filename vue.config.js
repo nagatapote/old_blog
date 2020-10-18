f59c3e633d4bc065119a2d4e86277ef81cafa60f
nagatapote <hiroaki29p@icloud.com>
2020-10-18 15:29:31 +0900

    pwa

--- vue.config.js
+++ vue.config.js
@@ -4,5 +4,25 @@ module.exports = {
       entry: 'src/main.js',
       title: 'pote\'s blog',
     }
+  },
+  pwa: {
+    workboxOptions: {
+      runtimeCaching: [
+        {
+          urlPattern: /https:nagatapote.work/,
+          handler: 'networkFirst',
+          options: {
+            cacheName: 'pote-cache',
+            expiration: {
+              maxEntries: 10,
+              maxAgeSeconds: 300
+            },
+            cacheableResponse: {
+              statuses: [0, 200]
+            }
+          }
+        }
+      ]
+    }
   }
 }
