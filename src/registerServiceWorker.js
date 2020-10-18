314029adf4c50bb5b0fd9472c2b3fd0374d56d86
nagatapote <hiroaki29p@icloud.com>
2020-10-18 15:13:47 +0900

    offline

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -28,3 +28,50 @@ if (process.env.NODE_ENV === 'production') {
     }
   })
 }
+// キャッシュするリソース(css、jsがあれば個別で追加)
+var resourcesToCache = [
+  '/'
+]
+
+// キャッシュバージョン
+var CACHE_VERSION = 'ca-v1'
+// キャッシュに追加
+self.addEventListener('install', function (event) {
+  console.log('Service Worker Install ')
+  event.waitUntil(
+    caches.open(CACHE_VERSION)
+      .then(function (cache) {
+        console.log('cache.addAll')
+        cache.addAll(resourcesToCache)
+      })
+  )
+})
+
+// キャッシュ表示
+var CACHE_DISP_VERSION = 'ca-d-v1'
+self.addEventListener('fetch', function (event) {
+  console.log('Service Worker fetch')
+  event.respondWith(
+    // キャッシュが存在するかチェック
+    caches.match(event.request)
+      .then(function (response) {
+        if (response) {
+          return response
+        } else {
+          // キャッシュがない場合キャッシュに入れる
+          return fetch(event.request)
+            .then(function (res) {
+              return caches.open(CACHE_DISP_VERSION)
+                .then(function (cache) {
+                  console.log('cache put')
+                  cache.put(event.request.url, res.clone())
+                  return res
+                })
+            })
+            .catch(function () {
+              // エラーが発生しても何もしない
+            })
+        }
+      })
+  )
+})
