2cd925cdd9f65735a33572b2f5c89087bc96e925
nagatapote <hiroaki29p@icloud.com>
2020-10-18 11:25:49 +0900

    blog_re

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,7 @@
   <center>
   <h1>pote's blog</h1>
   <h2>〜技術ブログ〜</h2>
-  <img src="../assets/top.jpeg" decoding="async">
+  <img src="../assets/top.png" decoding="async">
   </center>
   </div>
 </template>
